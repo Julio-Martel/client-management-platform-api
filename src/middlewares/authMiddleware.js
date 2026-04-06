2a7f0b863566b1f1a6a5ed099da22b4472b9d9c9
Julio-Martel <julioramdom123@gmail.com>
2026-04-06 19:10:02 -0300

    fix: variable rows

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,20 +1,24 @@
 const db = require('../config/db');
 
-const login = (req,res,next) => {
+const login = async(req,res,next) => {
     const {usuario, pass} = req.body;
 
     try {
-        
+        const [rows] = await db.query('SELECT * FROM Clientes WHERE usuario = ? AND  pass = ?',[usuario,pass]);
 
-
-
+        if(rows.length === 0){
+            return res.status(404).json({
+                mensaje: "Usuario no encontrado"
+            })
+        }
 
     } catch(Error){
-
+        res.status(500).json('Error del servidor');
     }
 
+    next();
 
 }
 
 
-module.exports = login;
+module.exports = {login};
--- src/routes/clientes.routes.js
+++ src/routes/clientes.routes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const clienteController = require('../controllers/cliente.controller');
+const middleware = require('../middlewares/authMiddleware');
 
 router.get('/', clienteController.getCliente);
-router.post('/', clienteController.createCliente);
+router.post('/login', middleware.login , clienteController.createCliente);
 router.delete('/:id',clienteController.deleteCliente);
 
 module.exports = router;
