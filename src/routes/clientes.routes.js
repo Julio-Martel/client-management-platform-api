42d07ba2e195d3d53977e3d1581ed05f02459e0b
Julio-Martel <julioramdom123@gmail.com>
2026-04-10 01:27:46 -0300

    refactor: agrega middlewares y controllers

--- src/routes/clientes.routes.js
+++ src/routes/clientes.routes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 const clientesController = require('../controllers/cliente.controller');
 const authMiddleware = require('../middlewares/authMiddleware');
-//const adminMiddleware = require('../middlewares/adminMiddleware');
-//const authController = require('../controllers/authController');
+const adminMiddleware = require('../middlewares/adminMiddleware');
 
-router.post('/', clientesController.createCliente);
+router.get('/', authMiddleware.authMiddleware, adminMiddleware.soloAdmin, clientesController.getCliente);
+router.post('/', authMiddleware.authMiddleware, adminMiddleware.soloAdmin, clientesController.createCliente);
+router.patch('/:id', authMiddleware.authMiddleware, adminMiddleware.soloAdmin, clientesController.updateCliente );
+
 
 module.exports = router;
